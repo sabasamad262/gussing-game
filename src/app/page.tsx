3b795f8edb36b04461b00fa9082803e91d6553e6
saba samad <sabasamad95@gmail.com>
2024-12-16 16:11:00 +0500

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NumberGuessing from '@/components/number-guessing'
+
+const page = () => {
+  return (
+    <div>
+      <NumberGuessing />
+    </div>
+  )
+}
+
+export default page
